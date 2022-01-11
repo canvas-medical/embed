49a3a99566b7f593f7eaed65d3e9819a11a616f4
Nick Telsan <nick.telsan@viget.com>
2022-01-11 15:20:15 -0500

    Date Select
    - Adds date select component and styles

---
+++ widgets/packages/scheduler/src/components/date-select/index.js
@@ -0,0 +1,65 @@
+import { h } from 'preact'
+
+import {
+  DateHeading,
+  DateScrollButton,
+  DateSelectButton,
+  DateViewContainer,
+  IconContainer,
+} from './styles'
+
+import {
+  ArrowBack,
+  ArrowForward,
+  Box,
+  Calendar,
+  isToday,
+  Span,
+  formatDate,
+  styles,
+} from '@canvas/common'
+
+export const DateSelect = ({ date, colors }) => {
+  const backDisabled = isToday(date)
+  const userTimezone = new Date()
+    .toLocaleTimeString('en-us', { timeZoneName: 'short' })
+    .split(' ')[2]
+
+  return (
+    <Box mt="16px">
+      <DateViewContainer backgroundColor={colors.accent}>
+        <DateScrollButton
+          disabled={backDisabled}
+          onClick={() => console.log("I'll send us back a day")}
+        >
+          <IconContainer>
+            <ArrowBack
+              fill={
+                backDisabled ? null : colors.primary || styles.default.primary
+              }
+            />
+          </IconContainer>
+        </DateScrollButton>
+
+        <DateSelectButton>
+          <IconContainer>
+            <Calendar />
+          </IconContainer>
+          <DateHeading>{formatDate(date)}</DateHeading>
+        </DateSelectButton>
+
+        <DateScrollButton
+          onClick={() => console.log("I'll send us forward a day")}
+        >
+          <IconContainer>
+            <ArrowForward fill={styles.default.primary} />
+          </IconContainer>
+        </DateScrollButton>
+      </DateViewContainer>
+
+      <Box mt="12px" mb="16px">
+        <Span>{`Appointment times shown in ${userTimezone}`}</Span>
+      </Box>
+    </Box>
+  )
+}
