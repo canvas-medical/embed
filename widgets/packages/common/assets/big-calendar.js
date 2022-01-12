cea454cc2c4078e2bd17a8843dca01c2a890db68
Jeremy Fields <jeremy.fields@viget.com>
2022-01-12 14:20:01 -0700

    [#14] Streamline calendar icon

---
+++ widgets/packages/common/assets/big-calendar.js
@@ -0,0 +1,8 @@
+import {
+  Calendar,
+} from '@canvas/common'
+import { h } from 'preact'
+
+export const BigCalendar = () => (
+  <Calendar height="36" width="34" />
+)
--- widgets/packages/common/assets/index.js
+++ widgets/packages/common/assets/index.js
@@ -1,3 +1,4 @@
 export * from './arrow-back'
 export * from './arrow-forward'
+export * from './big-calendar'
 export * from './calendar'
