8e39c1195d2bbbf9165c6e2e9360c48349e896ba
Nick Telsan <nick.telsan@viget.com>
2022-01-18 16:44:25 -0500

    Application Utilities
    - Reworks application context to include all potential props
    - Adds dummy props to host page

--- widgets/packages/scheduler/src/hooks/app-context.js
+++ widgets/packages/scheduler/src/hooks/app-context.js
@@ -1,13 +1,51 @@
-import { createContext } from 'preact'
-import { useContext } from 'preact/hooks'
+import { h, createContext } from 'preact'
+import { useContext, useState, useMemo } from 'preact/hooks'
 
 export const AppContext = createContext({
+  bailoutURL: null,
   shadowRoot: null,
+  patientId: null,
+  providerIds: null,
+  locationId: null,
+  appointmentTypeCode: null,
+  treatment: null,
+  reason: null,
+  duration: null,
   colors: {
     primary: null,
     accent: null,
     focus: null,
   },
+  screen: null,
+  setScreen: () => {},
+  date: null,
+  setDate: () => {},
 })
+
+export const ContextWrapper = ({ children, values }) => {
+  const [screen, setScreen] = useState('SELECT')
+  const [date, setDate] = useState(new Date())
+  const [timeSlot, setTimeSlot] = useState({
+    start: null,
+    end: null,
+    provider: null,
+  })
+
+  const contextValue = useMemo(() => {
+    return {
+      ...values,
+      screen,
+      setScreen,
+      date,
+      setDate,
+      timeSlot,
+      setTimeSlot,
+    }
+  }, [values, screen, setScreen, date, setDate, timeSlot])
+
+  return (
+    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+  )
+}
 
 export const useAppContext = () => useContext(AppContext)
