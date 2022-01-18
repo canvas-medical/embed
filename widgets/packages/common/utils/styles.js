69c76f2d7a476d39bac7e54dc9f7a4adcf0845e3
Nick Telsan <nick.telsan@viget.com>
2022-01-18 14:27:32 -0500

    Styling Utilities
    - Adds style strings for importing into styled components
    - Adds secondary button colors (red)
    - Adds (temporary) hover button colors

--- widgets/packages/common/utils/styles.js
+++ widgets/packages/common/utils/styles.js
@@ -7,11 +7,18 @@ export const styles = {
     primary: '#2185d0',
     accent: '#e8f2fa',
     focus: '#0d2c4c',
+    hover: '#0d2c4c',
   },
   buttons: {
     primary: {
       background: '#29933C',
       focus: '#0E3214',
+      hover: '#0E3214',
+    },
+    secondary: {
+      background: '#D02121',
+      focus: '#470B0B',
+      hover: '#470B0B',
     },
   },
 }
@@ -27,3 +34,27 @@ export const generateColors = brandColor => {
     focus: null,
   }
 }
+
+export const primaryBackgoundColor = `
+  background-color: var(--bg, ${styles.default.primary});
+`
+
+export const accentBackgoundColor = `
+  background-color: var(--bg, ${styles.default.accent});
+`
+
+export const hoverAndFocusColor = `
+  &:focus {
+    background-color: var(--fc, ${styles.default.focus});
+  }
+  &:hover {
+    background-color: var(--hc, ${styles.default.hover});
+  }
+`
+
+export const margin = `
+  margin-top: var(--mt, var(--my, var(--m, 0)));
+  margin-bottom: var(--mb, var(--my, var(--m, 0)));
+  margin-left: var(--ml, var(--mx, var(--m, 0)));
+  margin-right: var(--ml, var(--mx, var(--m, 0)));
+`
