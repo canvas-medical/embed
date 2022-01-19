aaaecee75890c1ebfd797d136985941d703bede4
Nick Telsan <nick.telsan@viget.com>
2022-01-19 13:07:50 -0500

    Scheduler Confirmation Screen
    - Adjusts styling of components for confirmation screen

--- widgets/packages/common/components/typography.js
+++ widgets/packages/common/components/typography.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
+import { margin } from '../utils'
 
 export const H2 = styled.h2`
+  ${margin}
+
   font-size: 1.125rem;
   font-weight: 700;
 `
 
 export const H3 = styled.h3`
+  ${margin}
+
   font-size: 1rem;
+  font-weight: 700;
 `
 
 export const LargerSpan = styled.span`
+  ${margin}
+
   font-size: 1rem;
 `
