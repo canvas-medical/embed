b3d6c31d1ddea3b6439280c462fd59a29e69e93d
Noah Over <noahko96@gmail.com>
2022-01-24 09:40:46 -0500

    [#22] Clean up code around endpoints

--- proxy/app/controllers/schedule_controller.rb
+++ proxy/app/controllers/schedule_controller.rb
@@ -3,6 +3,7 @@ class ScheduleController < ApplicationController
   before_action :valid_patient_check
 
   def index
-    render_fhir_response(client.get("/Schedule", ENV["FHIR_BASE_URL"]))
+    fhir_response = client.get("/Schedule", ENV["FHIR_BASE_URL"])
+    render_fhir_response(fhir_response)
   end
 end
