4660ef63eb796da086fc1c1ed52b18b4a975cc55
Nick Telsan <nick.telsan@viget.com>
2022-01-26 16:22:09 -0500

    [#15] Calendar
    - Adds Close icon
    - Adjusts existing icons to accept fill prop

--- widgets/packages/common/assets/arrow-forward.js
+++ widgets/packages/common/assets/arrow-forward.js
@@ -5,7 +5,14 @@ export const ArrowForward = ({ fill }) => (
     height="16"
     viewBox="0 0 10 16"
     width="10"
-    xmlns="http://www.w3.org/2000/svg">
-      <path d="m862.034779 91.5989021v9.9999999h10" fill="none" stroke={fill || '#fff'} stroke-width="2" transform="matrix(-.70710678 .70710678 .70710678 .70710678 545.815212 -672.793008)"/>
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="m862.034779 91.5989021v9.9999999h10"
+      fill="none"
+      stroke={fill || '#fff'}
+      stroke-width="2"
+      transform="matrix(-.70710678 .70710678 .70710678 .70710678 545.815212 -672.793008)"
+    />
   </svg>
 )
