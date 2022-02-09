a47935c964b7d84da108bd79b6b6780f8a75be54
Nick <nick.telsan@viget.com>
2022-02-09 10:56:52 -0500

    [#51] Loading State (#88)
    - Adds styles to loader

--- widgets/packages/common/components/loader.js
+++ widgets/packages/common/components/loader.js
@@ -1,4 +1,32 @@
 import { h } from 'preact'
-import { Box } from './boxes'
+import styled, { keyframes } from 'styled-components'
+import { Box } from '.'
 
-export const Loader = () => <Box style={{ '--mt': '3rem' }}>Loading</Box>
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Spinner = styled.div`
+  animation: ${rotate} 1.5s linear infinite;
+  animation-play-state: inherit;
+  border: solid 5px #e9f3fa;
+  border-bottom-color: #2185d0;
+  border-radius: 50%;
+  content: '';
+  height: 48px;
+  width: 48px;
+  transform: translate3d(-50%, -50%, 0);
+  will-change: transform;
+`
+
+export const Loader = () => (
+  <Box style={{ '--mt': '40%' }}>
+    <Spinner />
+  </Box>
+)
