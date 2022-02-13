094e563bcb08317eeb3cf6267132c68e267f6cb7
Nick Telsan <nick.telsan@viget.com>
2022-02-13 14:50:26 -0500

    Typescript Refactor - Create Appointment
    - Adds POST request for creating appointment
    - Adds function to construct POST body
    - Adds and adjusts types and constants

--- clients/common/src/api/index.ts
+++ clients/common/src/api/index.ts
@@ -1,1 +1,2 @@
 export * from './get-time-slots'
+export * from './post-appointment'
--- clients/common/src/utils/types/index.ts
+++ clients/common/src/utils/types/index.ts
@@ -12,3 +12,14 @@ export type ParsedSlotsType = {
   provider: ProvidersType
   providerSlots: SlotType[]
 }
+
+export type TreatmentType = {
+  type: string
+  code: string
+}
+
+export type TimeSlotType = {
+  start: string
+  end: string
+  provider: ProvidersType
+}
