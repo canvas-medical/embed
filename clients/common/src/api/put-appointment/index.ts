cc93059ede097c870e547551a2a6a92cbfb88592
Nick Telsan <nick.telsan@viget.com>
2022-02-16 16:42:29 -0500

    Put Appointment Refactor
    - Moves PUT Appointment/ into its own folder
    - Adds more explicit tyeps to function

---
+++ clients/common/src/api/put-appointment/index.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { statuses } from '../../utils'
+import { constructBody } from '../construct-body'
+import { PutAppointmentParamsType } from './types'
+
+export const putAppointment = ({
+  onComplete,
+  setError,
+  setLoading,
+  appointmentCoding,
+  description,
+  locationId,
+  timeSlot,
+  patientId,
+  patientKey,
+  api,
+  appointmentId,
+}: PutAppointmentParamsType) => {
+  setLoading(true)
+  const data = constructBody({
+    status: statuses.cancelled,
+    appointmentCoding,
+    description,
+    locationId,
+    timeSlot,
+    patientId,
+  })
+
+  axios
+    .put(`${api}/Appointment/${appointmentId}`, data, {
+      params: {
+        patient: patientId,
+        patient_key: patientKey,
+      },
+    })
+    .then(() => onComplete())
+    .catch(() => setError('Error Cancelling Appointment'))
+}
