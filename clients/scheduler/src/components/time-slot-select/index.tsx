e5e5ec8759e9eca2707a752b77f899fbe642d81a
Nick Telsan <nick.telsan@viget.com>
2022-02-18 12:21:10 -0500

    Fixes date navigation

--- clients/scheduler/src/components/time-slot-select/index.tsx
+++ clients/scheduler/src/components/time-slot-select/index.tsx
@@ -5,12 +5,12 @@ import { useAppContext } from '../../hooks'
 import { Ui } from './ui'
 
 export const TimeSlotSelect = () => {
-  const { fetchTimeSlots } = useAppContext()
+  const { fetchTimeSlots, date } = useAppContext()
   const [timeSlots, setTimeSlots] = useState<ParsedSlotsType[]>([])
 
   useEffect(() => {
     fetchTimeSlots(setTimeSlots)
-  }, [])
+  }, [date])
 
   return <Ui timeSlots={timeSlots} />
 }
