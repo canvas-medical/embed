6939dbf95c2486a724871c973b442df991746cc6
Tristan Lehmbecker <tristan@foundryinteractive.com>
2022-05-10 08:29:14 -0700

    useMemo update

--- clients/scheduler/src/components/time-slot-select/index.tsx
+++ clients/scheduler/src/components/time-slot-select/index.tsx
@@ -1,48 +1,30 @@
 import { h } from 'preact'
 import { isSameDay, ParsedSlotsType } from '@canvas-medical/embed-common'
-import { useState, useEffect } from 'preact/hooks'
+import { useState, useEffect, useMemo } from 'preact/hooks'
 import { useAppContext } from '../../hooks'
 import { Ui } from './ui'
 
 export const TimeSlotSelect = () => {
   const { fetchTimeSlots, date } = useAppContext()
-  const [timeSlots, setTimeSlots] = useState<ParsedSlotsType[]>([])
-  const [dayOfTimeSlots, setDayOfTimeSlots] = useState<ParsedSlotsType[]>([])
-  const [maxDate, setMaxDate] = useState<Date>(date)
+  const [providerTimeSlots, setProviderTimeSlots] = useState<ParsedSlotsType[]>([])
 
-  const addTimeSlots = (newSlots: ParsedSlotsType[]) => {
-    const udpatedSlots = newSlots.map((newSlot) => {
-      const existingProvider = timeSlots.find((timeSlot) => timeSlot.providerId === newSlot.providerId)
-      const mergedSlots = existingProvider ? [...existingProvider.providerSlots, ...newSlot.providerSlots] : newSlot.providerSlots
-      return {providerId: newSlot.providerId, providerSlots: mergedSlots}
+  const addTimeSlots = (newProviderTimeSlots: ParsedSlotsType[]) => {
+    const mergedProviderAvailability = newProviderTimeSlots.map((newProvider) => {
+      const provider = providerTimeSlots.find((entry) => entry.providerId === newProvider.providerId)
+      const mergedSlots = provider ? [...provider.providerSlots, ...newProvider.providerSlots] : newProvider.providerSlots
+      return {providerId: newProvider.providerId, providerSlots: mergedSlots}
     })
-    setTimeSlots(udpatedSlots)
+
+    setProviderTimeSlots(mergedProviderAvailability)
   }
 
-  useEffect(() => {
-    if (date >= maxDate) {
-      fetchTimeSlots(addTimeSlots)
-    }
-  }, [date])
-
-  useEffect(() => {
-    setDayOfTimeSlots(timeSlots.map((provider) => {
-      return {
-        providerId: provider.providerId,
-        providerSlots: provider.providerSlots.filter((slot) => {
-          return isSameDay(new Date(slot.start), date)
-        })
-      }
-    }))
-  }, [date, timeSlots])
-
   // Determine max date available for all providers
-  useEffect(() => {
-    if (timeSlots.length === 0) {
+  const maxDate = useMemo(() => {
+    if (providerTimeSlots.length === 0) {
       return
     }
 
-    const providersMaxDates = timeSlots.map((provider) => {
+    const providersMaxDates = providerTimeSlots.map((provider) => {
       if (provider.providerSlots.length === 0) {
         return undefined
       }
@@ -61,11 +43,26 @@ export const TimeSlotSelect = () => {
     })
 
     if (maxDateSlot) {
-      setMaxDate(new Date(maxDateSlot.start))
+      return new Date(maxDateSlot.start)
     }
-  }, [timeSlots])
+  }, [providerTimeSlots])
 
+  useEffect(() => {
+    if (typeof maxDate === "undefined" || date >= maxDate) {
+      fetchTimeSlots(addTimeSlots)
+    }
+  }, [date])
 
+  const dayOfTimeSlots = useMemo(() => {
+    return providerTimeSlots.map((provider) => {
+      return {
+        providerId: provider.providerId,
+        providerSlots: provider.providerSlots.filter((slot) => {
+          return isSameDay(new Date(slot.start), date)
+        })
+      }
+    })
+  }, [date, providerTimeSlots])
 
   return <Ui timeSlots={dayOfTimeSlots} />
 }
