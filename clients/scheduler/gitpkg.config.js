e244407ee9d7640408217442d83eee982a21df7c
Louis K <louis@caripay.net>
2022-09-01 12:42:03 -0400

    Rm gitpkg config

--- clients/scheduler/gitpkg.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = () => ({
-  registry: 'https://github.com/modern-age/canvas-embed.git',
-  // registry: 'git@github.com:modern-age/canvas-embed.git',
-  getTagName: pkg => {
-    return `canvas-medical/embed-scheduler-v${pkg.version}-gitpkg-josh-for-the-win`}
-})
