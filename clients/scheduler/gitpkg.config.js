9425a3b2a833b15f7f40e0de700d797a0b00e237
Louis K <louis@caripay.net>
2022-09-01 12:07:43 -0400

    hard code package name in gitpkg config for sched

--- clients/scheduler/gitpkg.config.js
+++ clients/scheduler/gitpkg.config.js
@@ -1,6 +1,6 @@
 module.exports = () => ({
-  // registry: 'https://github.com/modern-age/canvas-embed.git',
-  registry: 'git@github.com:modern-age/canvas-embed.git',
+  registry: 'https://github.com/modern-age/canvas-embed.git',
+  // registry: 'git@github.com:modern-age/canvas-embed.git',
   getTagName: pkg => {
-    return `${pkg.name}-v${pkg.version}-gitpkg-josh-for-the-win`}
+    return `canvas-medical/embed-scheduler-v${pkg.version}-gitpkg-josh-for-the-win`}
 })
