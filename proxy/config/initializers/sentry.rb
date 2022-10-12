ad9e5401dc5ccc334a44224dd19bb429b30d8b5c
Jeff Brower <jeffbrower0@gmail.com>
2022-10-12 13:49:01 -0400

    Fetch the Sentry DSN URL via env variable (#160)

--- proxy/config/initializers/sentry.rb
+++ proxy/config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = Rails.application.credentials.sentry_url
+  config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 0.0 to capture 0%
