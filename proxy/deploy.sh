a73c4a8cd326413a1a61f81c2040f0d1e5111004
Beau Gunderson <beau@beaugunderson.com>
2022-10-12 15:56:53 -0700

    cleanup

--- proxy/deploy.sh
+++ proxy/deploy.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-set -x
-
-APP_NAME="modern-age-proxy-dev"
+APP_NAME="$1"
 
 REPOSITORY_URI=004266138750.dkr.ecr.us-west-2.amazonaws.com/canvasmedical/embed_proxy:latest
 REPOSITORY_NAME=canvasmedical/embed_proxy
@@ -38,9 +36,6 @@ function push_to_ecr () {
       DOCKER_COMMAND="sudo docker"
   fi
 
-  # Use coreutils for grep and sed if available (usually for MacOS compatibility)
-  SED_COMMAND="$(command -v gsed >/dev/null && echo gsed || echo sed)"
-
   # Use the tag name if it exists... (Command needs to evaluate to 0 to not kill the shell.)
   GIT_COMMIT_BRANCH=$(git describe --tags --exact-match || echo 2> /dev/null)
 
@@ -48,12 +43,6 @@ function push_to_ecr () {
   if [[ -z "$GIT_COMMIT_BRANCH" ]]; then
     GIT_COMMIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   fi
-
-  # Slugify the branch name to make it docker tag friendly.
-  # BRANCH_SLUG=$(echo "$GIT_COMMIT_BRANCH" | iconv -t "ascii//TRANSLIT" | $SED_COMMAND -r "s/[^a-zA-Z0-9]+/-/g" | $SED_COMMAND -r "s/^-+\|-+$//g" | tr "[:upper:]" "[:lower:]")
-
-    # --cache-from "$REPOSITORY_NAME:master" \
-    # --cache-from "$REPOSITORY_NAME:$BRANCH_SLUG" \
 
   $DOCKER_COMMAND build \
     --pull \
@@ -63,7 +52,6 @@ function push_to_ecr () {
     --file "$DOCKERFILE" \
     .
 
-  # $DOCKER_COMMAND tag "$REPOSITORY_NAME:latest" "$REPOSITORY_URI"
   $DOCKER_COMMAND push "$REPOSITORY_URI"
 }
 
